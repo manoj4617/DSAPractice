7bd2e8d8bd6703ab3a9b55ceccc08c1acc54e7b7
Manojkumar B B <63506938+manoj4617@users.noreply.github.com>
2022-09-26 21:13:34 +0530

    dp file added

---
+++ DSA_Practice/DynamicProgramming/pascal_tri.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> pascal_triangle(int num_rows){
+    vector<vector<int>> p_tri;
+
+    for(int i=0;i<=num_rows;++i){
+        p_tri.push_back(vector<int>(i+1,1));
+        for(int j=1;j<i;++j){
+            p_tri[i][j] = p_tri[i-1][j] + p_tri[i-1][j-1];
+        }
+    }
+    vector<int> res;
+        for(int i=0;i<p_tri[num_rows].size();++i){
+            res.push_back(p_tri[num_rows][i]);
+        }
+        return res;
+    return res;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> res = pascal_triangle(n);
+    return 0;
+}
